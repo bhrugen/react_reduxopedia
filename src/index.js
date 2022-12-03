03a94b5f41f029a9dc94c1d733455d1da6d9f3d6
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-03 09:58:24 -0600

    Add Redux Store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./app/layout/Header";
-
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
+    <Provider store={store}>
+      <Header />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    //we will fill this reducer in the upcoming videos
+  },
+});
